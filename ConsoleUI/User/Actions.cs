094aba397352c80450e20f46e51dfe019cb9b577
SolverShade <seconduacc07@gmail.com>
2022-02-25 12:43:11 -0500

    A couple of files were missing despite the merge but were able to be added back by copy pasting them from previous commits

---
+++ ConsoleUI/User/Actions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.User
+{
+    public static class Actions
+    {
+        const int ACTIONLINE = 26;
+        public static void WritePossibleActions()
+        {
+            UILineEdit.setGuiLines(ACTIONLINE, 0);
+            Console.Write("Possible Actions: n (North), e (East), s (South), w (West) ");
+        }
+    }
+}
