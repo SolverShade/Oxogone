570018bde9a495131efe88ba3f5ad18d1484538e
SolverShade <seconduacc07@gmail.com>
2022-04-04 09:26:46 -0400

    Added listener to prevent windoww from being resized so that the custom text position of screen text does not move during resize.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,28 +1,34 @@
 ﻿#region usingStatements 
-
-#endregion
-
 using ConsoleUI.GameState;
 using ConsoleUI.Menu;
+using ConsoleUI.Window;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+#endregion
 
 namespace ConsoleUI
 {
     class Program
-    {
+    {       
         static void Main(string[] args)
         {
-            LoadArea loadArea = new LoadArea();
-            loadArea.GenerateWorld();
+            WindowListener.PreventResize();
+            StartGame();
 
             //MainMenu.WriteMenu();
             //MainMenu.Command();
 
             Console.ReadLine();
+        }
+
+        static void StartGame()
+        {
+            LoadArea loadArea = new LoadArea();
+            loadArea.GenerateWorld();
         }
 
 
